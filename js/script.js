27301a5a75b82b8e08286fb369557e80737c996e
Kamila Sporek <kamilasporek@gmail.com>
2020-11-24 19:41:22 +0100

    updateJS

--- js/script.js
+++ js/script.js
@@ -33,12 +33,12 @@ const titleClickHandler = function(event){
     
     /* [IN PROGRESS] find the correct article using the selector (value of 'href' attribute) */
   
-    const targetArticle = document.querySelector('.article .href');
+    const targetArticle = document.querySelector(articleSelector);
 
 
     /* add class 'active' to the correct article */
 
-    /*targetArticle.classList.add('active');*/
+    targetArticle.classList.add('active');
 }
   
     const links = document.querySelectorAll('.titles a');
